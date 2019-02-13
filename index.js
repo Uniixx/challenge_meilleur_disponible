6430615bc212bbc83ba5bed84622d508fd7f787a
Uniixx <42749804+Uniixx@users.noreply.github.com>
2019-02-12 21:49:36 -0500

    Update 2.0
    Refait à neuf, why not?

--- index.js
+++ index.js
@@ -2,63 +2,50 @@
 require("colors");
 const stdin = process.openStdin();
 
-const logStuff = message => {
-    console.log(message);
-};
-
-const setChar = (n, m, i, j, type) =>
-    ({
-        1: i === 1 || i === n || j === 1 || j === m ? "x" : "-",
-        2:
-            i === 1 || i === n || j === m || j === m || i === j || j === 1
-                ? "x"
-                : "-",
-        3:
-            i === 1 ||
-                i === n ||
-                j === m ||
-                j === m ||
-                i === j ||
-                j === n - i + 1 ||
-                j === 1
-                ? "x"
-                : "-"
-    }[type]);
-
-const square = (n, m, type) => {
-    let char = "";
-    let i, j;
-    for (i = 1; i <= n; i++) {
-        for (j = 1; j <= m; j++) {
-            char += setChar(n, m, i, j, type);
+class Shape {
+    constructor(width, heigth, type) {
+        this.width = width;
+        this.height = heigth;
+        this.type = type;
+    }
+    setChar({ opt1, opt2, opt3 }) {
+        if (opt1)
+            return 'x';
+        if (this.type === 2 && opt2)
+            return 'x';
+        if (this.type === 3 && opt3)
+            return 'x';
+        return '-';
+    }
+    Draw() {
+        let char = "";
+        let i, j;
+        for (i = 1; i <= this.height; i++) {
+            for (j = 1; j <= this.width; j++) {
+                char += this.setChar(
+                    {
+                        opt1: i === 1 || i === this.height || j === this.width || j === 1,
+                        opt2: i === j,
+                        opt3: i === j || j === this.height - i + 1
+                    },
+                    this.type
+                );
+            }
+            char += "\n";
         }
-        char += "\n";
+        return `${char}\n> Hacker be like <`;
     }
-    logStuff(`${char.green}
-${"> Hacker be like".green} `);
-};
+}
 
-logStuff(`Method 1: 1
-Method 2: 2
-Method 3: 3
-Leave: 4
-    `);
+const log = message => console.log(message);
 
-stdin.addListener("data", function (input) {
-    input = parseInt(input);
-    if (input === 1 || input === 2 || input === 3) {
-        square(20, 20, input);
-    } else if (input === 4) {
-        logStuff(
-            `♪ Goodbye my lover.
-Goodbye my friend.
-You have been the one.
-You have been the one for me. ♪`.magenta
-        );
-        process.exit(0);
-    } else {
-        logStuff(
-            `Wtf my dude? There is only 4 options come on.. shouldn't be that hard to choose :@`.red
-        );
-    }
+log("Method 1: 1\nMethod 2: 2\nMethod 3: 3\nLeave: 4");
+stdin.addListener("data", input => {
+  input = parseInt(input);
+  if (input <= 3) 
+      log(new Shape(20, 20, input).Draw().green);
+  else if (input === 4)
+      log(`♪ Goodbye my lover.\nGoodbye my friend.\nYou have been the one.\nYou have been the one for me. ♪`.magenta) + process.exit(0);
+  else
+      log(`Wtf my dude? There is only 4 options come on.. shouldn't be that hard to choose :@`.red);
 });
